07265f397c86b5bcee43346337ca73cc8b5f7734
ainansalsa <ainansalsa03@gmail.com>
2024-03-05 08:49:03 -0800

    editing routing dan controller

---
+++ editing routing dan controller/app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/About', 'Page::about');
+$routes->get('/Contact', 'Page::contact');
+$routes->get('/Faqs', 'Page::faqs');
